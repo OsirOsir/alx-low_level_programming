b55b249e01ae3c52048b7577bbfb66127f3d4955
Philip Osir <philiphenril@gmail.com>
2023-06-16 07:51:53 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints our a line with printf function
+ * Return:0 (Which is a Succes)
+ */
+init main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
