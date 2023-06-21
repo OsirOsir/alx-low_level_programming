eb240098b68f050d9ce56e1dddd7d76c3e74ec95
Philip Osir <philiphenril@gmail.com>
2023-06-21 02:49:53 -0700

    Update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
-	}
-	else if (n == 0)
-	{
-		-putchar(48);
-		return (0);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
+		-putchar(45);
+		return (-1);
 	}
-	return (-1);
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
