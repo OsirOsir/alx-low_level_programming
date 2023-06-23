6296ceb08a380c58f10e18b64909c76631c9ba7b
Philip Osir <philiphenril@gmail.com>
2023-06-23 07:53:38 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,18 @@
 
 /**
  * _isupper - check if a latter is upper
- * @x: The number to be checked
- * Return: 1 for upper letter or 0 for any else
+ * @c: The number to be checked
+ * Return: (1) for upper letter or (0) for any else
  */
 
-int_isupper(int x)
+int_isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
-		return (1);
+	return (1);
 	}
+	else
+	{
 	return (0);
+	}
 }
