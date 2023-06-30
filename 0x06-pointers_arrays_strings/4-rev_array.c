e24aa0dcfd5912802eea4b695810701e774d027d
Philip Osir <philiphenril@gmail.com>
2023-06-30 10:48:17 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,13 +5,14 @@
  * @a: The array of intergers to be reversed.
  * @n: The numbe of elements in the array.
  */
+
 void reverse_array(int *a, int n)
 {
 	int tmp, index;
 
 	for (index = n - 1; index >= n / 2; index--)
 	{
-		tmp = s[n - 1 - index];
+		tmp = a[n - 1 - index];
 		a[n - 1 - index] = a[index];
 		a[index] = tmp;
 	}
